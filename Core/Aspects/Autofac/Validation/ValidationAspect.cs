0401738c2c9ac1848a5ce0e4b643c4023af5d7ea
Furkann35 <yucelfurkan124@gmail.com>
2021-05-25 22:48:20 +0300

    ValidationAspect eklendi testleri yapıldı

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -20,7 +20,7 @@ namespace Core.Aspects.Autofac.Validation
             }
 
             _validatorType = validatorType;
-        }
+        } 
         protected override void OnBefore(IInvocation invocation)
         {
             var validator = (IValidator)Activator.CreateInstance(_validatorType);
